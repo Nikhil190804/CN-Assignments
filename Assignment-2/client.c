2b3a7ad65ee241870d1336f7a46a35dbccbed348
Nikhil <nikhil22322@iiitd.ac.in>
2024-09-20 14:29:47 -0400

    temp client code for testing connection

---
+++ Assignment-2/client.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    const char *hello = "Hello from client";
+    char buffer[1024] = {0};
+
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(3000);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address/Address not supported");
+        return -1;
+    }
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection Failed");
+        return -1;
+    }
+
+    // Send a message to the server
+    send(sock, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+
+    // Receive a message from the server
+    read(sock, buffer, 1024);
+    printf("Message received: %s\n", buffer);
+
+    // Close the socket
+    close(sock);
+    
+    return 0;
+}
