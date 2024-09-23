457620183e707cc95d116b309d479fc36e841ee8
Nikhil <nikhil22322@iiitd.ac.in>
2024-09-23 18:37:53 +0530

    done with client and updated makefile

--- Assignment-2/client.c
+++ Assignment-2/client.c
@@ -1,45 +1,87 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <arpa/inet.h>
 
-int main() {
-    int sock = 0;
-    struct sockaddr_in serv_addr;
-    const char *hello = "Hello from client";
-    char buffer[1024] = {0};
+typedef struct {
+    int thread_num;
+} client_args;
+
+void *client_function(void *args) {
+    client_args *cargs = (client_args *)args;
+    int sock;
+    struct sockaddr_in server_addr;
+    char buffer[2048];
+    memset(buffer, 0, 2048);
 
     // Create socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket creation error");
-        return -1;
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        perror("Could not create socket");
+        pthread_exit(NULL);
     }
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(3000);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(3000);
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        perror("Invalid address/Address not supported");
-        return -1;
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection failed");
+        close(sock);
+        pthread_exit(NULL);
     }
 
-    // Connect to the server
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Connection Failed");
-        return -1;
+    printf("Client %d connected to server.\n", cargs->thread_num);
+
+    const char *request = "GET_TOP_PROCESSES";
+    if (send(sock, request, strlen(request), 0) < 0) {
+        perror("Send failed");
+        close(sock);
+        pthread_exit(NULL);
     }
 
-    // Send a message to the server
-    send(sock, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
+    int recv_size = recv(sock, buffer, 2048-1, 0);
+    if (recv_size < 0) {
+        perror("Receive failed");
+        close(sock);
+        pthread_exit(NULL);
+    }
 
-    // Receive a message from the server
-    read(sock, buffer, 1024);
-    printf("Message received: %s\n", buffer);
+    buffer[recv_size] = '\0'; 
 
-    // Close the socket
+    printf("Client %d received data:\n%s\n", cargs->thread_num, buffer);
+
     close(sock);
-    
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <number_of_clients>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int n = atoi(argv[1]);
+    if (n <= 0) {
+        printf("Number of clients must be positive.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    pthread_t threads[n];
+    client_args args[n];
+
+    for (int i = 0; i < n; i++) {
+        args[i].thread_num = i + 1;
+        if (pthread_create(&threads[i], NULL, client_function, (void *)&args[i]) != 0) {
+            perror("Could not create thread");
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
     return 0;
 }
